3713bdb3af970d425f845ece334cb7dc94b56e96
hooni <hooni@samadal-PC>
2015-07-08 15:18:36 +0900

    2015.07.08. 3일차 종료 if-else까지

--- JavaOperator/src/ifCondition/IfElse.java
+++ JavaOperator/src/ifCondition/IfElse.java
@@ -1,14 +1,14 @@
 package ifCondition;
 
-public class OnlyIf {
+public class IfElse {
 	public static void main(String[] args) {
 		int x = 1, y = 2; // 지변 초기화
 		if ((x==1) && (y == 1)) { // && 엔퍼센트라 읽는다 (연산자)
 			System.out.println("x는 1 이면서, y도 2 이다.");
-		}
-		if ((x==1)||(y==1)) { // || 컬럼이라 읽는다 (연산자)
+		}else if ((x==3)||(y==3)) { // || 컬럼이라 읽는다 (연산자)
 			System.out.println("x는 1 이거나, y는 2 이다.");
+		}else{
+			System.out.println("해당 사항이 없습니다.");
 		}
 	}
-
 }
